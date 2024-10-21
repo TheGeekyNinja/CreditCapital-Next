e10683fb863ae3cf7f32a50e5bcbfc276ab1c73c
John Alesi <resurreccionjohnalesi@gmail.com>
2024-10-21 14:01:47 +0800

    fixed css

--- src/components/DashboardNavbar.tsx
+++ src/components/DashboardNavbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const DashboardNavbar = () => {
   return (
     <header className="flex justify-between items-center border-b-[0.5px] bg-white border-[#D0D5DD] p-4 px-6">
-      <Link href="/" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors rounded-full">
+      <Link href="/" className="px-4 py-2 bg-blue-500 text-white hover:bg-blue-600 transition-colors rounded-full">
         Home
       </Link>
       <div className="flex items-center space-x-4">
