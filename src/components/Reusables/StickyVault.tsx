563fc5d10068d368f2e2218f24557399b1a514c1
Graham Jenkins <graham@thegeeky.ninja>
2024-11-01 13:14:05 -0700

    Temp hack: Hardcode vault token balance.

--- src/components/Reusables/StickyVault.tsx
+++ src/components/Reusables/StickyVault.tsx
@@ -9,6 +9,7 @@ const StickyVault = () => {
   const PHTG_TOKEN_ADDRESS = process.env.NEXT_PUBLIC_PHTG_TOKEN_ADDRESS as `0x${string}`;
   const [goldPrice, setGoldPrice] = useState(0);
 
+  const hardcodedPhtgBalance = 744444000000000000000000;
   const { data: phtgBalance } = useReadContract({
     address: PHTG_TOKEN_ADDRESS,
     abi,
@@ -33,19 +34,23 @@ const StickyVault = () => {
     fetchGoldPrice();
   }, []);
 
-  const phtgAmount = phtgBalance ? Number(formatUnits(phtgBalance, 18)) : 0;
+  const phtgAmount = phtgBalance ? Number(formatUnits(BigInt(hardcodedPhtgBalance), 18)) : 0;
   const totalValueUSD = phtgAmount * goldPrice;
 
   console.log('PHTG Balance:', phtgAmount);
   console.log('Gold Price:', goldPrice);
   console.log('Total Value USD:', totalValueUSD);
 
-  return (
+  if (totalValueUSD === 0) {
+    return null;
+  } else {
+    return (
     <div className="bg-[#FFE79F] text-center py-1 text-lg font-medium text-[#666666]">
       Current Vault Holdings:
       <span className="text-black text-lg font-bold">${totalValueUSD.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
-    </div>
-  );
-}
+      </div>
+    );
+  }
+};
 
 export default StickyVault;
